c165901bc5bbfeb3bd4ad8a978a2acb6e35484ff
miltonferreira <jotonferreira@gmail.com>
2019-11-07 14:12:43 -0300

    Criando classe Order com Instant ISO 8601

--- src/main/java/com/jotonferreira/course/entities/User.java
+++ src/main/java/com/jotonferreira/course/entities/User.java
@@ -1,13 +1,18 @@
 package com.jotonferreira.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_user") // nome dado a classe User no BD
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
@@ -19,6 +24,10 @@ public class User implements Serializable{
 	private String email;
 	private String phone;
 	private String password;
+	
+	// pega a dependencia "client" na classe Order
+	@OneToMany(mappedBy = "client") // indica que um cliente tem vários pedidos 
+	private List<Order> orders = new ArrayList<>(); // lista de pedidos do cliente
 	
 	public User() {}
 
@@ -69,6 +78,10 @@ public class User implements Serializable{
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
