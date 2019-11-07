7e63e6b21b7dcdc0d3f084546b374898d2b1ae04
miltonferreira <jotonferreira@gmail.com>
2019-11-07 10:20:42 -0300

    BD H2 , test profile e JPA

--- src/main/java/com/jotonferreira/course/entities/User.java
+++ src/main/java/com/jotonferreira/course/entities/User.java
@@ -2,10 +2,19 @@ package com.jotonferreira.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id //indica que a chave primaria da classe
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //indica que vai ser auto-implementavel no banco de dados o valor do ID
 	private Long id;
+	
 	private String name;
 	private String email;
 	private String phone;
@@ -87,6 +96,4 @@ public class User implements Serializable{
 		return true;
 	}
 	
-	
-
 }
