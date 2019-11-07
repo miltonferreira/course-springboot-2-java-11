d3a5181a45158427890144084a03e058ec3a11e8
miltonferreira <jotonferreira@gmail.com>
2019-11-07 18:45:39 -0300

    criando o Enum orderStatus

--- src/main/java/com/jotonferreira/course/entities/Order.java
+++ src/main/java/com/jotonferreira/course/entities/Order.java
@@ -12,6 +12,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.jotonferreira.course.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order") // nome dado a classe Order no BD
@@ -25,16 +26,19 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT") // formata forma de mostra o tempo
 	private Instant moment;
 	
+	private Integer orderStatus;
+	
 	@ManyToOne // indica muitos pedidos para um cliente
 	@JoinColumn(name = "client_id") //nome da chave-estrangeira 
 	private User client; // dependencia para a classe User
 	
 	public Order() {}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -52,6 +56,17 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		
+		if(orderStatus != null) { // evita que passe um valor nulo ao orderStatus
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
